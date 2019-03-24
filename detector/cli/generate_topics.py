c8ab76b8cb7019739f6d617c2b84c81540a4579e
Eemeli Saari <saari.eemeli@gmail.com>
2019-03-24 14:54:01 +0200

    Refactored the topic generation cli tool

--- detector/cli/generate_topics.py
+++ detector/cli/generate_topics.py
@@ -6,7 +6,9 @@ import datetime
 import click
 
 sys.path.append('..')
-sys.path.append('../sHDP')
+sys.path.append('../sHDP/')
+
+print(sys.path)
 
 from corpora import Corpora
 from pipeline.embeddings import Word2VecWrapper, load_vectors
@@ -17,16 +19,16 @@ from pipeline.topics import (LDAWrapper, SHDPWrapper, document_topics,
 @click.command()
 @click.option('--model', type=str)
 @click.option('--alpha', type=int, default=1)
-@click.option('--n_topics', type=int)
+@click.option('--n_topics', type=int, default=50)
 @click.option('--data_path', type=str)
 @click.option('--data_prefix', type=str)
-@click.option('--result_path', type=str)
+@click.option('--result_path', type=str, default='')
 @click.option('--dictionary_path', type=str, default='')
 @click.option('--vectors_path', type=str)
-@click.option('--batch_size', type=int)
-@click.option('--iterations', type=int)
-@click.option('--passes', type=int)
-@click.option('--n_words', type=int)
+@click.option('--batch_size', type=int, default=10)
+@click.option('--iterations', type=int, default=500)
+@click.option('--passes', type=int, default=1)
+@click.option('--n_words', type=int, default=15)
 @click.option('--shuffle', '-s', is_flag=True)
 def main(model, alpha, n_topics, data_path, data_prefix, result_path, dictionary_path, 
          vectors_path, batch_size, iterations, passes, n_words, shuffle):
@@ -39,16 +41,13 @@ def main(model, alpha, n_topics, data_path, data_prefix, result_path, dictionary
         prefix=data_prefix,
         iterator='bow',
         word_up_limit=0.75, 
-        word_low_limit=20,
-        shuffle=shuffle
+        word_low_limit=20
     )
 
     if os.path.exists(dictionary_path):
         corpora = Corpora(dictionary=dictionary_path, **corpora_params)
     else:
-        print('BUILDING!')
         corpora = Corpora(**corpora_params).build()
-        print('built!')
         corpora.dictionary.save_as_text(dictionary_path)
 
     if len(corpora) == 0:
@@ -72,7 +71,7 @@ def main(model, alpha, n_topics, data_path, data_prefix, result_path, dictionary
                 passes=passes,
                 batch_size=batch_size,
                 batch_shuffle=shuffle,
-                vector_map=load_vectors(vectors_path),
+                vector_map=load_vectors(vectors_path, dictionary=corpora.dictionary),
                 num_docs=len(corpora)
             )
         )
@@ -89,8 +88,6 @@ def main(model, alpha, n_topics, data_path, data_prefix, result_path, dictionary
     if shuffle:
         model_name += f'_shuffled'
 
-    model_name += datetime.date.today().strftime("%d-%m-%Y-%H-%M-%S")
-
     path_dir = os.path.join(result_path, model_name)
     if not os.path.exists(path_dir):
         os.mkdir(path_dir)
@@ -99,10 +96,19 @@ def main(model, alpha, n_topics, data_path, data_prefix, result_path, dictionary
         topic_model.save(model_name)
 
     topic_df = document_topics(topic_model, corpora)
-    words_df = model_words(topic_model, n=n_words)
     
-    topics_path = os.path.join(path_dir, 'topics_' + model_name + '.csv', index=False)
-    words_path = os.path.join(path_dir, 'words_' + model_name + '.csv', index=False)
+    dictionary = None
+    if model == 'shdp':
+        dictionary = corpora.dictionary
+
+    words_df = model_words(topic_model, n=n_words, dictionary=dictionary)
+
+    topics_path = os.path.join(path_dir, 'topics_' + model_name + '.csv')
+    words_path = os.path.join(path_dir, 'words_' + model_name + '.csv')
+
+    print(topic_df.head())
+    print(topic_df.tail())
+    print(topic_df['year'].unique())
 
     topic_df.to_csv(topics_path, index=False)
     words_df.to_csv(words_path, index=False)
