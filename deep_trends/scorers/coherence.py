647ca4b01a564974d0908f63ca83c930cf9dc217
Eemeli Saari <saari.eemeli@gmail.com>
2019-04-29 21:15:28 +0300

    Fix the coherence computations

--- deep_trends/scorers/coherence.py
+++ deep_trends/scorers/coherence.py
@@ -1,33 +1,38 @@
 from collections import defaultdict
 from itertools import chain
 
+import numpy as np
 from gensim.topic_coherence import segmentation, direct_confirmation_measure, text_analysis
 
-def inverted_accumulator(corpus, relevant_words):
+
+def inverted_accumulator(corpus, relevant_words, dictionary=None):
     """
 
     """
+    if dictionary is None:
+        dictionary = corpus.dictionary
+
     inverted_index = defaultdict(set)
     id2contiguous = {word_id: i for i, word_id in enumerate(relevant_words)}
     for i, (t, _) in enumerate(corpus):
-        filtered = list(filter(lambda w: w in relevant_words, corpus.dictionary.doc2idx(t)))
+        filtered = list(filter(lambda w: w in relevant_words, dictionary.doc2idx(t)))
         for idx in filtered:
             inverted_index[id2contiguous[idx]].add(i)
 
-    accumulator = text_analysis.InvertedIndexAccumulator(relevant_words, dictionary=corpus.dictionary)
+    accumulator = text_analysis.InvertedIndexAccumulator(relevant_words, dictionary=dictionary)
     accumulator._inverted_index = inverted_index
     accumulator._num_docs = len(corpus)
-    
+
     return accumulator
 
 
-def pmi(topics, corpus):
+def pmi(topics, corpus, dictionary=None):
     """
 
     """
     unique_ids = unique_ids = set(chain(*topics))
 
-    accumulator = inverted_accumulator(corpus, unique_ids)
+    accumulator = inverted_accumulator(corpus, unique_ids, dictionary=dictionary)
 
     segments = segmentation.s_one_pre(topics)
 
